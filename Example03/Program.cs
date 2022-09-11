dfbe2978c0f5cf0315af474e9286731f3e528e77
Denis <d.lyskovtsev@mail.ru>
2022-09-11 18:54:25 +0400

    Homework3

---
+++ Example03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.Write("Введенное число четое");
+}
+else
+{
+    Console.Write("Введенное число нечётное");
+}
