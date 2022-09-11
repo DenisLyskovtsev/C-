38846b0c437238b61cac7d4ed3c2551d6420aa17
Denis <d.lyskovtsev@mail.ru>
2022-09-11 18:49:50 +0400

    Homework1

---
+++ Example01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число ");
+int numberA = int.Parse(Console.ReadLine()); 
+Console.WriteLine("Введите второе число ");
+int numberB = int.Parse(Console.ReadLine()); 
+int max = numberA;
+if (max < numberB) max = numberB;
+{
+    Console.WriteLine($"max= {max}");
+}
